d7c728909abc730ade16c16900643089c0550498
zhouyijun <zhou495248579@hotmail.com>
2023-10-22 20:15:27 +0800

    chore:init

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+export default {
+  setup() {
+    const count = ref(0);
+    return { count };
+  },
+};
+</script>
+
+<template>
+  <h1>header</h1>
+
+  <button
+    @click="
+      () => {
+        count++;
+      }
+    "
+  >
+    click
+  </button>
+  <div>count : {{ count }}</div>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
